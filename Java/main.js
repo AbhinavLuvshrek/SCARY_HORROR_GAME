97f1f6c70711ec795087e9cb93b356739d2e2551
AbhinavLuvshrek <113375203+AbhinavLuvshrek@users.noreply.github.com>
2023-01-12 11:22:23 -0700

    BDBDBD

---
+++ Java/main.js
@@ -0,0 +1,11 @@
+// variables
+let wuss = document.getElementById("ws");
+
+//Event Listeners
+wuss.addEventListener("click", selectWuss);
+
+//functions
+
+function selectWuss() {
+  window.close();
+}
